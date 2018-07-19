10af5bac82fa321a1ab924dad98da5479e9e5ca8
LXF <Rdxer@foxmail.com>
2018-07-19 08:57:19 +0800

    ✨ - init 脚本 修改，link 创建

--- init.sh
+++ init.sh
@@ -2,8 +2,17 @@
 
 echo 开始初始化项目
 
-cd Tools/maker
+cd Tools/PilotCLI
 
-# rmdir ./build
+echo 创建 xcodeproj
+
+swift package generate-xcodeproj
+
+echo 更新 CLI 工具
+
+swift package update
+
+echo 构建 CLI 工具
 
 swift build 
+
