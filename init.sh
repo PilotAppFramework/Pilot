4e13346bd8d8bf6b9e20e9111d21d234bcc6d699
LXF <Rdxer@foxmail.com>
2018-07-21 09:38:33 +0800

    ✨ - 添加创建启动文件指令

--- init.sh
+++ init.sh
@@ -2,17 +2,29 @@
 
 echo 开始初始化项目
 
+echo 创建启动文件 🆕
+
+cp App/App/Env/LauchMode.ini.example   App/App/Env/LauchMode.ini
+
+echo 创建启动文件  ✅  App/App/Env/LauchMode.ini
+
+
+echo 开始构建 CLI 工具 🆕
+
 cd Tools/PilotCLI
 
 echo 创建 xcodeproj
 
 swift package generate-xcodeproj
 
-echo 更新 CLI 工具
+echo 更新 CLI 工具  
 
 swift package update
 
-echo 构建 CLI 工具
+echo 编译 CLI 工具
 
 swift build 
+
+echo 构建 CLI 工具 结束 ✅
+
 
